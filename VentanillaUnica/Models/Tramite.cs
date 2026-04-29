117a866010905f8916864dab2a6df0157145cb34
Johan Pacheco <johanpach9@gmail.com>
2026-04-29 07:51:18 -0500

    lista tramites en select

--- VentanillaUnica/Models/Tramite.cs
+++ VentanillaUnica/Models/Tramite.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace VentanillaUnica.Models;
 
 public class Tramite
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    
     public string Nombre { get; set; } = string.Empty;
     public string Descripcion { get; set; } = string.Empty;
-    public int DiasEstimados { get; set; } // para calcular fecha fin estimada
+    public int DiasEstimados { get; set; }
     public bool Activo { get; set; } = true;
 
     public ICollection<Solicitud> Solicitudes { get; set; } = [];
